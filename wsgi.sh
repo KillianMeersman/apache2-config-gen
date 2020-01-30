8c82a3a0d1ba22c1f23fe00f5ce431682b6bf86d
Killian Meersman <contact@killianm.dev>
2020-01-30 23:19:40 +0100

    Improved SSL security

--- wsgi.sh
+++ wsgi.sh
@@ -38,6 +38,11 @@ Flags:
     shift
 done
 
+# Generate DHE key
+if [ -z /etc/ssl/certsdhparam.pem ]; then
+	openssl dhparam -out /etc/ssl/certsdhparam.pem 4096
+fi
+
 template="<VirtualHost *:80>
     ServerName $domain
 
@@ -65,6 +70,16 @@ template="<VirtualHost *:80>
 
     SSLCertificateFile $ssl_dir/$domain/fullchain.pem
     SSLCertificateKeyFile $ssl_dir/$domain/privkey.pem
+    SSLCompression off
+    SSLProtocol -all +TLSv1.3 +TLSv1.2
+    SSLCipherSuite EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:ECDHE-RSA-AES128-SHA:DHE-RSA-AES128-GCM-SHA256:AES256+EDH:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES128-SHA256:DHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES256-GCM-SHA384:AES128-GCM-SHA256:AES256-SHA256:AES128-SHA256:AES256-SHA:AES128-SHA:DES-CBC3-SHA:HIGH:!aNULL:!eNULL:!EXPORT:!DES:!MD5:!PSK:!RC4
+    SSLHonorCipherOrder on
+    SSLOpenSSLConfCmd Curves X25519:secp521r1:secp384r1:prime256v1
+    SSLOpenSSLConfCmd DHParameters '/etc/ssl/certs/dhparam.pem'
+
+    LoadModule headers_module modules/mod_headers.so
+    Header always set Strict-Transport-Security "max-age=63072000; includeSubdomains;"
+    Header always set X-Frame-Options DENY
 </VirtualHost>
 </IfModule>"
 
